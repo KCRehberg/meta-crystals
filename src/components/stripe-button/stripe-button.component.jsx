c0d03d60adaa0162e6be002be2fa2b7f20b7280f
KCRehberg <krehberg12@gmail.com>
2020-10-11 17:21:17 -0400

    integrated stripe into checkout for payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HbBVtJsXfm5EQHJcL64iTpqZHqQrUu9FfttaoFeXRqMjW2CtL8VoQI88LVNwZHBiOapYuBWimc8N3QPL1jeQ1dV0007f5AAgt';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Meta Crystals"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
